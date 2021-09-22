768ed0e7cc5ffcada1133c75bf2378006945f4da
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2021-09-22 23:48:59 +0100

    working now

--- code/server.bat
+++ code/server.bat
@@ -1,9 +1,7 @@
 
 taskkill /f /im chrome.exe 2> nul & ver > nul
 
-set url="http://127.0.0.1:8080/index2.html"
+set url="http://127.0.0.1:8080/index.html"
 start chrome %url% --auto-open-devtools-for-tabs
-
-rem start "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://127.0.0.1:8080" --auto-open-devtools-for-tabs
 
 http-server "./" -c-1 
